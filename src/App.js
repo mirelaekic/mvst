353f5b2b3073ff0bbceeff0897c1a9f064daacbe
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-06-11 22:50:18 +0200

    tab added
    finished CSS for tabs and added columns

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import Tab from './components/Tab';
 
 function App() {
   return (
     <div className="App">
-      github
+      <Container>
+      <Row>
+        <Col lg={3}>profile</Col>
+        <Col lg={8}>
+          <Tab />
+        </Col>
+      </Row>
+      </Container>
     </div>
   );
 }
