6a45edaf75d0f5161316a53e88e6967ac8c05668
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-06-13 15:25:27 +0200

    fixed styling

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -5,16 +5,16 @@ export default function Tab() {
   return (
     <Nav fill variant="tabs" defaultActiveKey="rep">
       <Nav.Item>
-        <Nav.Link eventKey="overview"><i className="bi bi-book"></i> Overview</Nav.Link>
+        <Nav.Link className="tab-item" eventKey="overview"><i className="bi bi-book"></i> Overview</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="rep"><i className="bi bi-journal-bookmark-fill"></i> Repositories</Nav.Link>
+        <Nav.Link className="tab-item" eventKey="rep"><i className="bi bi-journal-bookmark-fill"></i> Repositories</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="projects"><i className="bi bi-kanban"></i> Projects</Nav.Link>
+        <Nav.Link className="tab-item" eventKey="projects"><i className="bi bi-kanban"></i> Projects</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="packages"><i className="bi bi-box"></i> Packages</Nav.Link>
+        <Nav.Link className="tab-item" eventKey="packages"><i className="bi bi-box"></i> Packages</Nav.Link>
       </Nav.Item>
     </Nav>
   );
